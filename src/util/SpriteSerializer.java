eb4ced65f023c6a1b9712a6d7f2bfeb3d1c76a4f
Adithya <ra102@duke.edu>
2017-11-11 17:14:48 -0500

    gson status serialization util for shared use by authoring & play through authoring_engine and behavior modules

---
+++ src/util/SpriteSerializer.java
@@ -0,0 +1,9 @@
+package util;
+
+public class SpriteSerializer {
+
+	public SpriteSerializer() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/util/StatusSerializerDeserializer.java
@@ -0,0 +1,42 @@
+package util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class StatusSerializerDeserializer {
+
+	public StatusSerializerDeserializer() {
+	}
+		
+	// TODO
+	public String serializeStatus(Map<String, String> status) {
+		StringBuilder bob = new StringBuilder();
+		Gson gson = new GsonBuilder().create();
+		System.out.println("Serializing status");
+		gson.toJson(status, bob);
+		String serializedStatus = bob.toString();
+		System.out.println(serializedStatus);
+		return serializedStatus;
+	}
+	
+	public Map<String, String> deserializeStatus(String serializedStatus) {
+		Gson gson = new GsonBuilder().create();
+		return gson.fromJson(serializedStatus, Map.class);
+	}
+	
+	
+	public static void main(String[] args) {
+		StatusSerializerDeserializer tester = new StatusSerializerDeserializer();
+		Map<String, String> testMap = new HashMap<>();
+		testMap.put("a", "1");
+		String serializedMap = tester.serializeStatus(testMap);
+		testMap = tester.deserializeStatus(serializedMap);
+		for (String key : testMap.keySet()) {
+			System.out.print(key + " : " + testMap.get(key));
+		}
+	}
+
+}
