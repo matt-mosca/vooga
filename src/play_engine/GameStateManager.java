52fb0c655ebbe11006b9f1a398e7c656c28e07bf
Adithya <ra102@duke.edu>
2017-11-13 16:31:50 -0500

    Modify visibility of play_engine methods, leaving only top-level controller methods public and others at most package-private

--- src/play_engine/GameStateManager.java
+++ src/play_engine/GameStateManager.java
@@ -31,142 +31,69 @@ public class GameStateManager {
 		gameElements = new HashSet<>();
 	}
 	
-	/**
-	 * Run the game loop for the given number of cycles
-	 * 
-	 * @param cycles
-	 *            the number of cycles
-	 */
-	public void update() {
+	void update() {
 		// TODO
 	}
 
-	/**
-	 * Get lives left
-	 * 
-	 * @return number of lives left
-	 */
-	public int getLives() {
+	int getLives() {
 		// TODO
 		return 0; // TEMP
 	}
 
-	/**
-	 * Retrieve the amount of each resource left
-	 * 
-	 * @return map of resource name to amount left
-	 */
-	public Map<String, Integer> getResources() {
+	Map<String, Integer> getResources() {
 		// TODO
 		return new HashMap<>(); // TEMP
 	}
 
-	/**
-	 * Query the current level of the game
-	 * 
-	 * @return the integer corresponding to the game's current level
-	 */
-	public int getCurrentLevel() {
+	int getCurrentLevel() {
 		// TODO
 		return 1; // TEMP
 	}
 	
-	/**
-	 * Query current play status (lives, kills, resources, all top-level metrics)
-	 * @return map of parameter name to value
-	 */
-	public Map<String, String> getStatus() {
+	Map<String, String> getStatus() {
 		// TODO
 		return new HashMap<>();
 	}
 
-	/**
-	 * Query whether the game is currently in play
-	 * 
-	 * @return true if in play, false if over / paused
-	 */
-	public boolean isInPlay() {
+	boolean isInPlay() {
 		// TODO
 		return false; // TEMP
 	}
 
-	/**
-	 * Query whether game has been won
-	 * 
-	 * @return true if won, false otherwise
-	 */
-	public boolean isWon() {
+	boolean isWon() {
 		// TODO
 		return false; // TEMP
 	}
 
-	/**
-	 * Query whether game has been lost
-	 * 
-	 * @return true if lost, false otherwise
-	 */
-	public boolean isLost() {
+	boolean isLost() {
 		// TODO
 		return false; // TEMP
 	}
 
-	/**
-	 * Query whether the current level has been cleared (if so, game will be paused
-	 * until resume() is called )
-	 * 
-	 * @return true if current level is cleared and game is paused, false otherwise
-	 */
-	public boolean isLevelCleared() {
+	boolean isLevelCleared() {
 		// TODO
 		return false; // TEMP
 	}
 
-	/**
-	 * Pause the game
-	 */
-	public void pause() {
+	void pause() {
 		// TODO
 	}
 
-	/**
-	 * Resume the game
-	 */
-	public void resume() {
+	void resume() {
 		// TODO
 	}
 
-	/**
-	 * Called to get current collection of events
-	 * 
-	 * @return current collection of elements
-	 */
-	public Collection<Sprite> getCurrentElements() {
+	Collection<Sprite> getCurrentElements() {
 		return gameElements;
 	}
 
-	/**
-	 * Place element of specified name at specified location
-	 * 
-	 * @param elementName
-	 *            name of element which can be used by ElementFactory to construct a
-	 *            TowerDefenseElement using properties / json data files
-	 * @param x
-	 *            xCoordinate where element was placed
-	 * @param y
-	 *            yCoordinate where element was placed
-	 * @return the created element
-	 */
-	public Sprite placeElement(String elementName, double x, double y) {
+	Sprite placeElement(String elementName, double x, double y) {
 		// TODO
 		return null; // TEMP
 	}
 	
-	public void saveGameState(String savedGameName) {
+	void saveGameState(String savedGameName) {
 		playIOController.saveGameState(savedGameName, gameDescription, gameStatus, gameElements);
-	}
-	
-	void setDescription(String newDescription) {
-		gameDescription = newDescription;
 	}
 	
 	void setStatus(Map<String, String> newStatus) {
--- src/play_engine/PlayController.java
+++ src/play_engine/PlayController.java
@@ -67,7 +67,7 @@ public class PlayController {
 		gameStateManager.setStatus(loadedSettings);
 		return loadedSettings;
 	}
-
+	
 	/**
 	 * Fetch all available game names and their corresponding descriptions
 	 * 
@@ -75,6 +75,128 @@ public class PlayController {
 	 */
 	public Map<String, String> getAvailableGames() {
 		return playIOController.getAvailableGames();
+	}
+
+	
+	/**
+	 * Run the game loop for the given number of cycles
+	 * 
+	 * @param cycles
+	 *            the number of cycles
+	 */
+	public void update() {
+		gameStateManager.update();
+	}
+
+	/**
+	 * Get lives left
+	 * 
+	 * @return number of lives left
+	 */
+	public int getLives() {
+		return gameStateManager.getLives(); // TEMP
+	}
+
+	/**
+	 * Retrieve the amount of each resource left
+	 * 
+	 * @return map of resource name to amount left
+	 */
+	public Map<String, Integer> getResources() {
+		return gameStateManager.getResources();
+	}
+
+	/**
+	 * Query the current level of the game
+	 * 
+	 * @return the integer corresponding to the game's current level
+	 */
+	public int getCurrentLevel() {
+		return gameStateManager.getCurrentLevel();
+	}
+	
+	/**
+	 * Query current play status (lives, kills, resources, all top-level metrics)
+	 * @return map of parameter name to value
+	 */
+	public Map<String, String> getStatus() {
+		return gameStateManager.getStatus();
+	}
+
+	/**
+	 * Query whether the game is currently in play
+	 * 
+	 * @return true if in play, false if over / paused
+	 */
+	public boolean isInPlay() {
+		return gameStateManager.isInPlay();
+	}
+
+	/**
+	 * Query whether game has been won
+	 * 
+	 * @return true if won, false otherwise
+	 */
+	public boolean isWon() {
+		return gameStateManager.isWon();
+	}
+
+	/**
+	 * Query whether game has been lost
+	 * 
+	 * @return true if lost, false otherwise
+	 */
+	public boolean isLost() {
+		return gameStateManager.isLost();
+	}
+
+	/**
+	 * Query whether the current level has been cleared (if so, game will be paused
+	 * until resume() is called )
+	 * 
+	 * @return true if current level is cleared and game is paused, false otherwise
+	 */
+	public boolean isLevelCleared() {
+		return gameStateManager.isLevelCleared();
+	}
+
+	/**
+	 * Pause the game
+	 */
+	public void pause() {
+		gameStateManager.pause();
+	}
+
+	/**
+	 * Resume the game
+	 */
+	public void resume() {
+		gameStateManager.resume();
+	}
+
+	/**
+	 * Called to get current collection of events
+	 * 
+	 * @return current collection of elements
+	 */
+	public Collection<Sprite> getCurrentElements() {
+		return gameStateManager.getCurrentElements();
+	}
+
+	/**
+	 * Place element of specified name at specified location
+	 * 
+	 * @param elementName
+	 *            name of element which can be used by ElementFactory to construct a
+	 *            TowerDefenseElement using properties / json data files
+	 * @param x
+	 *            xCoordinate where element was placed
+	 * @param y
+	 *            yCoordinate where element was placed
+	 * @return the created element
+	 */
+	public Sprite placeElement(String elementName, double x, double y) {
+		return gameStateManager.placeElement(elementName, x, y);
 	}
 
 }
