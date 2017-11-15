595ea4b5b47d176da36362f8bf77f61d0e07dd2b
MATTHEW MOSCA <MJM141@duke.edu>
2017-11-15 18:45:19 -0500

    AuthoringGameTower.java created to represent towers in authoring environment

---
+++ src/authoring/AuthoringGameTower.java
@@ -0,0 +1,12 @@
+package authoring;
+
+import javafx.scene.image.ImageView;
+
+public class AuthoringGameTower {
+	ImageView graphic;
+	double xPosition;
+	double yPosition;
+	double height;
+	double width;
+	double firingRate;
+}
