e6c01ab7395f294e0aed1b284bcfe0dc916d493b
Venkat Subramaniam <venkat.subramaniam@duke.edu>
2017-11-16 17:10:16 -0500

    created properties file and deletable levels

---
+++ src/authoring/LevelModifier.java
@@ -0,0 +1,14 @@
+package authoring;
+
+import javafx.scene.layout.Pane;
+
+//popup window which allows one to modify different parameters in the level which they are editing
+public class LevelModifier extends Pane {
+	private int myLevel;
+	
+	
+	public LevelModifier(int level) {
+		myLevel = level;
+		
+	}
+}
