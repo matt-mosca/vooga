35f9e6da677c7e0ba77190cb367440afc274823c
Venkat Subramaniam <venkat.subramaniam@duke.edu>
2017-11-16 15:48:59 -0500

    creating lv toolbar

--- src/authoring/BottomToolBar.java
+++ src/authoring/BottomToolBar.java
@@ -1,12 +1,22 @@
 package authoring;
 
-public class BottomToolBar extends ToolBar{
+import javafx.collections.ObservableList;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
+public class BottomToolBar extends ToolBar{
+	private TableView<ObjectProperties> table = new TableView<ObjectProperties>();
+	private ObjectProperties[] dataArray;
+	private ObservableList<ObjectProperties> data;
+	private TableColumn<ObjectProperties, String> firstCol;
+	private TableColumn<ObjectProperties, String> lastCol;
 	
 	public BottomToolBar(AuthorInterface author) {
 		super(author);
 		this.setLayoutY(500);
 		this.setLayoutX(260);
+		
+		
 		
 	}
 	
