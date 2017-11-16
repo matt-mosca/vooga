7fe4f3b8b7bebb543d041c19db66a9ace63fb6c6
Venkat Subramaniam <venkat.subramaniam@duke.edu>
2017-11-16 15:35:47 -0500

    fun

---
+++ src/authoring/BottomToolBar.java
@@ -0,0 +1,13 @@
+package authoring;
+
+public class BottomToolBar extends ToolBar{
+
+	
+	public BottomToolBar(AuthorInterface author) {
+		super(author);
+		this.setLayoutY(500);
+		this.setLayoutX(260);
+		
+	}
+	
+}
---
+++ src/authoring/ToolBar.java
@@ -0,0 +1,11 @@
+package authoring;
+
+import javafx.scene.layout.VBox;
+
+public class ToolBar extends VBox{
+
+	public ToolBar(AuthorInterface author) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
