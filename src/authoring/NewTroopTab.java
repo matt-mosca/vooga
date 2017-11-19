87efbeb8abed4307c9199e7b0f195b3a65fd53e5
MATTHEW MOSCA <MJM141@duke.edu>
2017-11-19 15:13:31 -0500

    Properties files added and sprite tabs modified to incorporate them

--- src/authoring/NewTroopTab.java
+++ src/authoring/NewTroopTab.java
@@ -1,7 +1,21 @@
 package authoring;
 
+import java.util.ResourceBundle;
+
 public class NewTroopTab extends NewSpriteTab {
+	
+	private ResourceBundle troopResources;
+	
 	public NewTroopTab() {
 		super();
+		troopResources = ResourceBundle.getBundle("authoring/resources/NewTroopImages");
+		addDefaultImages();
+		updateImages();
+	}
+
+	@Override
+	protected void addDefaultImages() {
+		// TODO Auto-generated method stub
+		
 	}
 }
