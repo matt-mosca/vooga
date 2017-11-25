f728253ab78667a6ab6df33ea6ceb78c718fdee6
Ben Welton <bw144@duke.edu>
2017-11-24 20:06:17 -0700

    Left tool bar tabs implemented

--- src/authoring/BackgroundTab.java
+++ src/authoring/BackgroundTab.java
@@ -1,30 +1,39 @@
 package authoring;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import authoring.rightToolBar.NewSpriteTab;
-import interfaces.CreationInterface;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.ListView;
 import sprites.BackgroundObject;
+import sprites.StaticObject;
 
 public class BackgroundTab extends NewSpriteTab{
-	private BackgroundObject myBackground1;
-	private BackgroundObject myBackground2;
-	private BackgroundObject myBackground3;
-	private BackgroundObject myBackground4;
-	private BackgroundObject myBackground5;
 	private AuthorInterface author; 
+	private List<StaticObject> myList;
+	private ListView<StaticObject> myListView;
 	
 	public BackgroundTab(AuthorInterface ai) {
 		super(ai);
 		author = ai;
+		addDefaultImages();
 	}
 
 	@Override
 	protected void addDefaultImages() {
-		myBackground1 = createNewBackground(3, "grass_small.png");
-		myBackground2 = createNewBackground(3, "grass2_small.png");
-		myBackground3 = createNewBackground(2, "brick_path.png");
-		myBackground4 = createNewBackground(2, "stone_path1.png");
-		myBackground5 = createNewBackground(3, "water_medium.png");
+		myList = new ArrayList<>(Arrays.asList(createNewBackground(3, "grass_small.png"),
+				createNewBackground(3, "grass2_small.png"),createNewBackground(2, "brick_path.png"),
+				createNewBackground(2, "stone_path1.png"), createNewBackground(3, "water_medium.png")));
 		
+		ObservableList<StaticObject> items = FXCollections.observableArrayList(myList);
+		myListView = new ListView<>();
+		myListView.setOnMouseClicked(e->author.clicked(
+      		myListView.getSelectionModel().getSelectedItem()));
+		myListView.setItems(items);
+		this.setContent(myListView);
 	}
 	
 	private BackgroundObject createNewBackground(int size, String imageString) {
--- src/authoring/StaticTab.java
+++ src/authoring/StaticTab.java
@@ -1,6 +1,7 @@
 package authoring;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import authoring.rightToolBar.NewSpriteTab;
@@ -10,9 +11,6 @@ import javafx.scene.control.ListView;
 import sprites.StaticObject;
 
 public class StaticTab extends NewSpriteTab{
-	private StaticObject myStatic1;
-	private StaticObject myStatic2;
-	private StaticObject myStatic3;
 	private AuthorInterface author;
 	private List<StaticObject> myList;
 	private ListView<StaticObject> myListView;
@@ -20,18 +18,13 @@ public class StaticTab extends NewSpriteTab{
 	public StaticTab(AuthorInterface ai) {
 		super(ai);
 		author = ai;
+		addDefaultImages();
 	}
 
 	@Override
 	protected void addDefaultImages() {
-		myStatic1 = createNewStatic(1, "tortoise.png");
-		myStatic2 = createNewStatic(1, "gray_circle.png");
-		myStatic3 = createNewStatic(1, "green_soldier.gif");
-		
-		myList = new ArrayList<>();
-		myList.add(myStatic1);
-		myList.add(myStatic2);
-		myList.add(myStatic3);
+		myList = new ArrayList<>(Arrays.asList(createNewStatic(1, "tortoise.png"),
+				createNewStatic(1, "gray_circle.png"),createNewStatic(1, "green_soldier.gif")));
 		
 		ObservableList<StaticObject> items = FXCollections.observableArrayList(myList);
 		myListView = new ListView<>();
@@ -41,7 +34,7 @@ public class StaticTab extends NewSpriteTab{
 		this.setContent(myListView);
 	}
 	
-	private StaticObject createNewStatic(int size, String imageString) {
+	protected StaticObject createNewStatic(int size, String imageString) {
 		return new StaticObject(size, author, imageString);
 	}
 
