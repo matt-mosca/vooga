c9cd89fa22aae82663abcbecba654567b0e42d84
Ben Schwennesen <bas65@duke.edu>
2017-11-25 17:15:43 -0500

    Implement movement strategy interface in abstract movement strategy class

--- src/engine/behavior/movement/AbstractMovementStrategy.java
+++ src/engine/behavior/movement/AbstractMovementStrategy.java
@@ -1,39 +1,36 @@
 package engine.behavior.movement;
 
 /**
- * Use abstract class instead of interface to encapsulate x and y coordinate
- * data
+ * Use abstract class instead of interface to encapsulate x and y coordinate data
  * 
  * @author radithya
- *
  */
-public abstract class MovementStrategy implements IMovementStrategy {
+public abstract class AbstractMovementStrategy implements MovementStrategy {
 
 	private double xCoordinate;
 	private double yCoordinate;
 	private double speed;
 
-	public MovementStrategy(double startX, double startY, double speed) {
+	public AbstractMovementStrategy(double startX, double startY, double speed) {
 		xCoordinate = startX;
 		yCoordinate = startY;
 		this.speed = speed;
 	}
 
-	/**
-	 * Move based on the specific movement strategy
-	 */
+	@Override
 	public abstract void move();
 
-	/**
-	 * What to do when blocked
-	 */
+	@Override
 	public abstract void handleBlock();
 
 	/**
-	 * The current xCoordinate
-	 * 
-	 * @return current xCoordinate
+	 * Retrieve speed.
+	 *
+	 * @return the speed at which the object using this strategy should use.
 	 */
+	protected double getSpeed() { return speed; }
+
+	@Override
 	public double getX() {
 		return xCoordinate;
 	}
@@ -43,23 +40,13 @@ public abstract class MovementStrategy implements IMovementStrategy {
 		return yCoordinate;
 	}
 
-	/**
-	 * Set x-coordinate
-	 * 
-	 * @param newXCoord
-	 *            x-coordinate to set to
-	 */
-	protected void setX(double newXCoord) {
+	@Override
+	public void setX(double newXCoord) {
 		xCoordinate = newXCoord;
 	}
 
-	/**
-	 * Set y-coordinate
-	 * 
-	 * @param newYCoord
-	 *            y-coordinate to set to
-	 */
-	protected void setY(double newYCoord) {
+	@Override
+	public void setY(double newYCoord) {
 		yCoordinate = newYCoord;
 	}
 
