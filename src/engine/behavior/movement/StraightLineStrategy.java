7b7d3875e0c720ce6bb270eb902917a4c2ceb160
Ben Schwennesen <bas65@duke.edu>
2017-11-25 17:11:40 -0500

    Add simple straight line movement strategy

---
+++ src/engine/behavior/movement/StraightLineStrategy.java
@@ -0,0 +1,29 @@
+package engine.behavior.movement;
+
+import engine.behavior.ParameterName;
+import javafx.geometry.Point2D;
+
+public class StraightLineStrategy extends AbstractMovementStrategy {
+
+    private Point2D velocity;
+
+    public StraightLineStrategy(@ParameterName("startX")  double startX, @ParameterName("startY") double startY,
+                                @ParameterName("speed")   double speed, @ParameterName("targetX") double targetX,
+                                @ParameterName("targetY") double targetY) {
+        super(startX, startY, speed);
+        velocity = new Point2D(targetX, targetY);
+        velocity.normalize();
+        velocity.multiply(Math.sqrt(getSpeed()));
+    }
+
+    @Override
+    public void move() {
+        setX(getX() + velocity.getX());
+        setY(getX() + velocity.getY());
+    }
+
+    @Override
+    public void handleBlock() {
+        // TODO
+    }
+}
