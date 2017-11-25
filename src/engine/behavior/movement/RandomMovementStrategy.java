40827665e09bf9ef6917fe39b0914158a3db9ed2
Ben Schwennesen <bas65@duke.edu>
2017-11-25 17:16:12 -0500

    Refactor to make random movement strategy generation simpler

--- src/engine/behavior/movement/RandomMovementStrategy.java
+++ src/engine/behavior/movement/RandomMovementStrategy.java
@@ -1,5 +1,7 @@
 package engine.behavior.movement;
 
+import engine.behavior.ParameterName;
+
 import java.awt.geom.Point2D;
 
 /**
@@ -9,13 +11,17 @@ import java.awt.geom.Point2D;
  * @author radithya
  *
  */
-public final class RandomMovementStrategy extends MovementStrategy {
+public final class RandomMovementStrategy extends AbstractMovementStrategy {
 
 	private Point2D.Double chosenDirection;
 
-	public RandomMovementStrategy(double startX, double startY, RandomMovementAssigner randomMovementAssigner) {
-		super(startX, startY);
+	public RandomMovementStrategy(@ParameterName("startX") double startX, @ParameterName("startY") double startY,
+								  @ParameterName("speed") double speed,
+								  @ParameterName("directionProbabilities") double[] directionProbabilities) {
+		super(startX, startY, speed);
+		RandomMovementAssigner randomMovementAssigner = new RandomMovementAssigner(directionProbabilities);
 		chosenDirection = randomMovementAssigner.assignMovementDirection();
+		// TODO - set velocity vector to have norm == speed
 	}
 
 	@Override
