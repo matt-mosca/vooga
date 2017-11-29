55120d6505f7939170817c41ece34bc8a71f05ef
Michael Scruggs <mts40@duke.edu>
2017-11-29 04:34:49 -0500

    Added to movement strategies resourse file

--- src/engine/behavior/movement/PathFollowingMovementStrategy.java
+++ src/engine/behavior/movement/PathFollowingMovementStrategy.java
@@ -12,7 +12,7 @@ import javafx.geometry.Point2D;
  * @author mscruggs
  *
  */
-public abstract class PathFollowingMovementStrategy extends TargetedMovementStrategy {
+public class PathFollowingMovementStrategy extends TargetedMovementStrategy {
 	
 	private PathList coordinates;
 	private Point2D target;
