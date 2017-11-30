908e76bac7e5ae4eab61835cf3b1c7769437aefd
Ben Schwennesen <bas65@duke.edu>
2017-11-29 19:58:20 -0500

    Fix bugs packager

--- src/packaging/Packager.java
+++ src/packaging/Packager.java
@@ -1,12 +1,11 @@
 package packaging;
 
+import main.Main;
 import networking.ChatTestWindow;
 
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import javax.tools.JavaCompiler;
+import javax.tools.ToolProvider;
+import java.io.*;
 import java.util.jar.Attributes;
 import java.util.jar.JarEntry;
 import java.util.jar.JarOutputStream;
@@ -28,8 +27,6 @@ public class Packager {
     public static final int MAX_ENTRY_LENGTH = 1024;
     // no need for these in properties files since they will never change
     private final String WINDOWS_PATH_DELIMITER_PATTERN = Pattern.quote("\\");
-    private final String SRC = "src/";
-    private final String MAIN_CLASS_KEY = "Main-Class";
     private final String MANIFEST_VERSION = "1.0";
     private final String GAMES_ROOT = "data/games/";
     private final String JAR_EXTENSION = ".jar";
@@ -37,13 +34,15 @@ public class Packager {
     // this will eventually be all game engine classes, among other things to include like resource files
     // for now, they're just a bunch of files for testing purposes
     private final String TESTING_PACKAGE = "networking";
-    private final String[] FILES_TO_INCLUDE = {
-            TESTING_PACKAGE + File.separator + "ChatClient.class",
+    private final String[] FILES_TO_INCLUDE = { "src", };//"images", "resources", "authoring", "data" };
+           /* TESTING_PACKAGE + File.separator + "ChatClient.class",
             TESTING_PACKAGE + File.separator + "ChatThread.class",
             TESTING_PACKAGE + File.separator + "ChatTestWindow.class"
-    };
+    };*/
 
-    private final Class LAUNCH_CLASS = ChatTestWindow.class;
+    private final Class LAUNCH_CLASS = Main.class;
+
+    private JavaCompiler javaCompiler = ToolProvider.getSystemJavaCompiler();
 
     /**
      * Generate an executable JAR file for an authored game.
@@ -53,18 +52,36 @@ public class Packager {
      * @param gameName - the chosen name of the game
      */
     public void generateJar(String gameName) {
+        //compile(new File("src"));
+        //JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+        //compiler.run(null, null, null, new File("src/main/Main.java").getAbsolutePath());
         Manifest manifest = new Manifest();
         manifest.getMainAttributes().put(Attributes.Name.MANIFEST_VERSION, MANIFEST_VERSION);
-        manifest.getMainAttributes().putValue(MAIN_CLASS_KEY, LAUNCH_CLASS.getName());
+        manifest.getMainAttributes().put(Attributes.Name.MAIN_CLASS, ChatTestWindow.class.getName());
+        //manifest.getMainAttributes().put(Attributes.Name.CLASS_PATH, "netwo");
         try {
             FileOutputStream outputStream = new FileOutputStream(GAMES_ROOT + gameName + JAR_EXTENSION);
             JarOutputStream target = new JarOutputStream(outputStream, manifest);
-            for (String fileName : FILES_TO_INCLUDE) {
+            /*for (String fileName : FILES_TO_INCLUDE) {
                 addToJar(new File(fileName), target);
+            }*/
+            for (File directory : new File("src").listFiles()) {
+                addToJar(directory, target);
             }
             target.close();
         } catch (IOException e) {
             // TODO - report failure to build the game
+            e.printStackTrace();
+        }
+    }
+
+    private void compile(File source) {
+        if (source.isDirectory() && !source.getName().isEmpty()) {
+            for (File subdirectoryOrFile : source.listFiles()) {
+                compile(subdirectoryOrFile);
+            }
+        } else if (source.getName().endsWith(".java")){
+            javaCompiler.run(null, null, null, source.getAbsolutePath());
         }
     }
 
@@ -73,7 +90,7 @@ public class Packager {
     private void addToJar(File source, JarOutputStream target) throws IOException {
         if (source.isDirectory() && !source.getName().isEmpty()) {
             addDirectoryToJar(source, target);
-        } else {
+        } else if (!source.getName().endsWith(".java")){
             JarEntry entry = new JarEntry(convertPathToJarFormat(source.getPath()));
             entry.setTime(source.lastModified());
             target.putNextEntry(entry);
@@ -100,9 +117,10 @@ public class Packager {
     }
 
     private void writeToJar(File source, JarOutputStream target) throws IOException {
-        InputStream in = new BufferedInputStream(getClass().getClassLoader().getResourceAsStream(source.getPath()));
+        InputStream in = new BufferedInputStream(new FileInputStream(source));
         byte[] buffer = new byte[MAX_ENTRY_LENGTH];
         int count;
+        System.out.println(source.getPath());
         while ((count = in.read(buffer)) != -1) {
             target.write(buffer, 0, count);
         }
