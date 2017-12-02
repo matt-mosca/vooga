29b335a895c72348177cdb2c80628372b5be390b
Adithya <ra102@duke.edu>
2017-12-02 17:17:00 -0500

    ElementManager & PlayController refactors (remove redundant / duplicate references, split / reorganize methods, etc.)

--- src/engine/play_engine/ElementManager.java
+++ src/engine/play_engine/ElementManager.java
@@ -2,7 +2,6 @@ package engine.play_engine;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Predicate;
@@ -21,15 +20,12 @@ import sprites.SpriteFactory;
 public class ElementManager {
 
 	private SpriteFactory spriteFactory;
-
-	private final String TEAM_NUMBER_KEY = "Numerical \"team\" association", ZERO = "0";
-
 	// Use list to enforce an ordering of elements to facilitate consideration of
 	// every element pair only once
-	private List<Sprite> gameElements;
+	private List<Sprite> activeElements;
 	private List<Sprite> newElements;
 	private List<Sprite> deadElements;
-	
+
 	private SpriteQueryHandler spriteQueryHandler;
 
 	// TODO
@@ -39,60 +35,38 @@ public class ElementManager {
 	 * Handles the collision-checking and Sprite-specific collision-handling logic
 	 * Implements the 'Behavior' interface from the api/doc in the DESIGN_PLAN.md
 	 */
-	public ElementManager(SpriteFactory spriteFactory) {
+	public ElementManager(SpriteFactory spriteFactory, SpriteQueryHandler spriteQueryHandler) {
 		this.spriteFactory = spriteFactory;
+		this.spriteQueryHandler = spriteQueryHandler;
 		newElements = new ArrayList<>();
 		deadElements = new ArrayList<>();
-		spriteQueryHandler = new SpriteQueryHandler();
 	}
 
 	// Guaranteed to return only active elements (i.e. not dead ones)
 	Collection<Sprite> getCurrentElements() {
 		// Filter to return only active elements?
-		gameElements.removeIf(gameElement -> !gameElement.isAlive());
-		return gameElements;
+		activeElements.removeIf(gameElement -> !gameElement.isAlive());
+		return activeElements;
 	}
 
 	void setCurrentElements(List<Sprite> newElements) {
-		gameElements = newElements;
+		activeElements = newElements;
 	}
 
-	/*
-	 * AbstractMovementStrategy object should be created with the coordinates Method
-	 * might still be necessary but should just do void and put in authoring game
-	 * grid
-	 */
-
 	void update() {
-		newElements.clear();
-		for (int elementIndex = 0; elementIndex < gameElements.size(); elementIndex++) {
-			Sprite element = gameElements.get(elementIndex);
+		for (int elementIndex = 0; elementIndex < activeElements.size(); elementIndex++) {
+			Sprite element = activeElements.get(elementIndex);
 			element.move();
-			if (element.shouldFire()) {
-				String elementTemplateName = element.fire();
-				List<Sprite> exclusionOfSelf = new ArrayList<>(gameElements);
-				exclusionOfSelf.remove(element);
-				Map<String, String> spriteToGenerateTemplate = spriteFactory.getTemplateProperties(elementTemplateName);
-				int toGenerateId = Integer.parseInt(spriteToGenerateTemplate.getOrDefault(TEAM_NUMBER_KEY, ZERO));
-				Map<String, Object> auxiliaryObjects = spriteQueryHandler
-						.getAuxiliarySpriteConstructionObjectMap(element.getPlayerId(), new Point2D(element.getX(), element.getY()),
-						exclusionOfSelf);
-				Sprite projectileSprite = spriteFactory.generateSprite(elementTemplateName,
-						new Point2D(element.getX(), element.getY()), auxiliaryObjects);
-				System.out.println(element.fire());
-				System.out.println("X: " + element.getX());
-				System.out.println("Y: " + element.getY());
-				newElements.add(projectileSprite);
-			}
+			handleElementFiring(element);
 			processAllCollisionsForElement(elementIndex, element);
 		}
-		gameElements.forEach(element -> {
+		activeElements.forEach(element -> {
 			if (!element.isAlive() || (element.reachedTarget() && element.shouldRemoveUponCompletion())) {
 				deadElements.add(element);
 			}
 		});
-		gameElements.removeAll(deadElements);
-		gameElements.addAll(newElements);
+		activeElements.removeAll(deadElements);
+		activeElements.addAll(newElements);
 	}
 
 	List<Sprite> getDeadElements() {
@@ -103,9 +77,12 @@ public class ElementManager {
 		deadElements.clear();
 	}
 
-
 	List<Sprite> getNewlyGeneratedElements() {
 		return newElements;
+	}
+	
+	void clearNewElements() {
+		newElements.clear();
 	}
 
 	boolean allEnemiesDead() {
@@ -121,7 +98,7 @@ public class ElementManager {
 	}
 
 	boolean allElementsFulfillCondition(Predicate<Sprite> condition) {
-		for (Sprite element : gameElements) {
+		for (Sprite element : activeElements) {
 			if (!condition.test(element)) {
 				return false;
 			}
@@ -130,20 +107,29 @@ public class ElementManager {
 	}
 
 	private void processAllCollisionsForElement(int elementIndex, Sprite element) {
-		for (int otherIndex = elementIndex + 1; otherIndex < gameElements.size(); otherIndex++) {
-			Sprite otherElement = gameElements.get(otherIndex);
+		for (int otherIndex = elementIndex + 1; otherIndex < activeElements.size(); otherIndex++) {
+			Sprite otherElement = activeElements.get(otherIndex);
 			if (element.collidesWith(otherElement)) {
 				element.processCollision(otherElement);
 				otherElement.processCollision(element);
 			}
 		}
 	}
+	
+	private void handleElementFiring(Sprite element) {
+		if (element.shouldFire()) {
+			String elementTemplateName = element.fire();
+			List<Sprite> exclusionOfSelf = new ArrayList<>(activeElements);
+			exclusionOfSelf.remove(element);
+			// Use player id of firing element rather than projectile? This allows greater
+			// flexibility
+			Map<String, Object> auxiliaryObjects = spriteQueryHandler.getAuxiliarySpriteConstructionObjectMap(
+					element.getPlayerId(), new Point2D(element.getX(), element.getY()), exclusionOfSelf);
+			Sprite projectileSprite = spriteFactory.generateSprite(elementTemplateName,
+					new Point2D(element.getX(), element.getY()), auxiliaryObjects);
+			newElements.add(projectileSprite);
+		}
 
-	/*
-	 * Santo's GridManager is not ready, so might as well skip this for now and use
-	 * the exact collision-checking logic above // TEMP - SIMPLIFIED CHECKING OF
-	 * COLLISIONS, JUST BY GRID POSITION private boolean collidesWith(Sprite
-	 * element, Sprite otherElement) { // TODO return false; // TEMP }
-	 */
-
+	}
+	
 }
