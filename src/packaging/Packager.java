4c8e8b0969d143fcabca84694ce7b7a2a875eb7b
Ben Schwennesen <bas65@oit-link10.oit.duke.edu>
2017-12-02 22:48:15 -0500

    Add Publisher for posting exported games to Google Drive and creating sharable link; test the publisher and update packing tests

--- src/packaging/Packager.java
+++ src/packaging/Packager.java
@@ -1,16 +1,9 @@
 package packaging;
 
-import main.Main;
-import networking.ChatTestWindow;
-
 import javax.tools.JavaCompiler;
 import javax.tools.ToolProvider;
 import java.io.*;
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.Enumeration;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 import java.util.jar.Attributes;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
@@ -18,91 +11,153 @@ import java.util.jar.JarOutputStream;
 import java.util.jar.Manifest;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
-import java.util.zip.ZipInputStream;
+import java.util.zip.ZipException;
 
 /**
  * Packages up a game for delivery as an executable JAR.
- *
- * TODO - Currently packages everything; need to make Player extend Application to be able to have game itself launch.
- * TODO - extract raw strings 
  *
  * @author Ben Schwennesen
  */
 public class Packager {
 
-    private final int MAX_ENTRY_LENGTH = 1024;
     private final String WINDOWS_PATH_DELIMITER_PATTERN = Pattern.quote("\\");
     private final String MANIFEST_VERSION = "1.0";
-    private final String GAMES_ROOT = "data/games/";
     private final String JAR_EXTENSION = ".jar";
-    private final File SOURCE_DIRECTORY = new File("src");
-    private final Class LAUNCH_CLASS = Main.class;
+    private final String JAVA_EXTENSION = ".java";
+    private final String CLASS_EXTENSION = ".class";
+
+    private final int MAX_ENTRY_LENGTH = 2048;
 
     private JavaCompiler javaCompiler = ToolProvider.getSystemJavaCompiler();
+
+    private Set<File> filesToDelete = new HashSet<>();
 
     /**
      * Generate an executable JAR file for an authored game.
      *
-     * Based on https://stackoverflow.com/questions/1281229/how-to-use-jaroutputstream-to-create-a-jar-file.
-     *
-     * @param gameName - the chosen name of the game
+     * @param outputPath           output path for the exported game JAR file
+     * @param sourceDirectoryPath  path to the source code directory
+     * @param outDirectoryPath     path to the compiled code directory, needed for calls to getResourceAsStream()
+     * @param launchClass          class used to launch the JAR
+     * @param directoriesToInclude data or resource directories outside the source directory needed to run the game;
+     *                             note that resources obtained through getResourceAsStream() are already handled
+     * @throws IOException if one of the directories given does not exist or input is malformed
      */
-    public void generateJar(String gameName) {
-        compile(new File("src"));
+    public void generateJar(String outputPath, String sourceDirectoryPath, String outDirectoryPath, Class launchClass,
+                            String... directoriesToInclude) throws IOException {
+        File sourceDirectory = new File(sourceDirectoryPath);
+        compile(sourceDirectory);
+        JarOutputStream target = initializeJarOutputStream(outputPath, launchClass);
+        String pathToExcludeFromSourceEntries = addTrailingFileSeparatorToPath(sourceDirectoryPath);
+        addToJar(sourceDirectory, target, pathToExcludeFromSourceEntries, JAVA_EXTENSION);
+        String pathToExcludeFromOutEntries = addTrailingFileSeparatorToPath(outDirectoryPath);
+        addToJar(new File(outDirectoryPath), target, pathToExcludeFromOutEntries, CLASS_EXTENSION);
+        for (String directory : directoriesToInclude) {
+            addToJar(new File(directory), target, "", CLASS_EXTENSION);
+        }
+        target.close();
+        cleanUp();
+    }
+
+    private JarOutputStream initializeJarOutputStream(String outputPath, Class launchClass) throws IOException {
         Manifest manifest = new Manifest();
         manifest.getMainAttributes().put(Attributes.Name.MANIFEST_VERSION, MANIFEST_VERSION);
-        manifest.getMainAttributes().put(Attributes.Name.MAIN_CLASS, LAUNCH_CLASS.getName());
-        try {
-            FileOutputStream outputStream = new FileOutputStream(GAMES_ROOT + gameName + JAR_EXTENSION);
-            JarOutputStream target = new JarOutputStream(outputStream, manifest);
-            addToJar(SOURCE_DIRECTORY, target);
-            writeResources(target);
-            addLibraries(target);
-            target.close();
-        } catch (IOException e) {
-            // TODO - report failure to build the game
-            e.printStackTrace();
-        }
+        manifest.getMainAttributes().put(Attributes.Name.MAIN_CLASS, launchClass.getName());
+        FileOutputStream outputStream = new FileOutputStream(outputPath);
+        return new JarOutputStream(outputStream, manifest);
+    }
+
+    private String addTrailingFileSeparatorToPath(String path) {
+        return path.endsWith(File.separator) ? path : path + File.separator;
     }
 
     private void compile(File source) {
-        if (source.isDirectory() && !source.getName().isEmpty()) {
-            for (File subdirectoryOrFile : source.listFiles()) {
-                compile(subdirectoryOrFile);
+        if (source.isDirectory()) {
+            File[] sourceFiles = source.listFiles();
+            if (sourceFiles != null) {
+                for (File subdirectoryOrFile : sourceFiles) {
+                    compile(subdirectoryOrFile);
+                }
             }
-        } else if (source.getName().endsWith(".java")){
+        } else if (source.getName().endsWith(JAVA_EXTENSION)) {
             javaCompiler.run(null, null, null, source.getAbsolutePath());
+            filesToDelete.add(new File(source.getAbsolutePath().replace(JAVA_EXTENSION, CLASS_EXTENSION)));
         }
     }
 
-    // This and its called methods are based on
-    // https://stackoverflow.com/questions/1281229/how-to-use-jaroutputstream-to-create-a-jar-file
-    private void addToJar(File source, JarOutputStream target) throws IOException {
-        if (source.isDirectory() && !source.getName().isEmpty()) {
-            addDirectoryToJar(source, target);
-        } else if (source.getName().endsWith(".class")){
-            writeFileToJar(target, source, "src/");
+    private void cleanUp() {
+        filesToDelete.removeIf(file -> !file.getPath().endsWith(CLASS_EXTENSION));
+        filesToDelete.forEach(File::delete);
+    }
+
+    private void addToJar(File source, JarOutputStream target, String excludeFromPath, String extensionToIgnore)
+            throws IOException {
+        if (source.getPath().endsWith(JAR_EXTENSION)) {
+            addExternalLibrary(source, target);
+        } else if (source.isDirectory()) {
+            addDirectoryToJar(source, target, excludeFromPath, extensionToIgnore);
+        } else if (!source.getPath().endsWith(extensionToIgnore)) {
+            addFileToJar(target, source, excludeFromPath);
         }
     }
 
-    private void addDirectoryToJar(File source, JarOutputStream target) throws IOException {
-        String name = convertPathToJarFormat(source.getPath());
-        if (!name.endsWith(File.separator)) {
-            name += File.separator;
+    private void addExternalLibrary(File libraryFile, JarOutputStream target) throws IOException {
+        try {
+            JarFile jarFile = new JarFile(libraryFile);
+            Stream<JarEntry> jarEntryStream = jarFile.stream();
+            jarEntryStream.forEach(jarEntry -> addLibraryJarEntry(target, jarFile, jarEntry));
+        } catch (ZipException couldNotOpenZipException) {
+            // ignore -- thrown because the "library file" is actually the target JAR
         }
-        JarEntry entry = new JarEntry(name.replace("src/", ""));
-        entry.setTime(source.lastModified());
-        target.putNextEntry(entry);
-        target.closeEntry();
+    }
+
+    private void addLibraryJarEntry(JarOutputStream target, JarFile jarFile, JarEntry jarEntry) {
+        try {
+            target.putNextEntry(jarEntry);
+            writeFileToJar(jarFile.getInputStream(jarEntry), target);
+            target.closeEntry();
+        } catch (IOException entryAlreadyAddedException) {
+            // ignore -- already added
+        }
+    }
+
+    private void addDirectoryToJar(File source, JarOutputStream target, String excludeFromPath,
+                                   String extensionToIgnore) throws IOException {
+        writeDirectoryJarEntry(source, target, excludeFromPath);
         File[] nestedFilesAndDirectories = source.listFiles();
         if (nestedFilesAndDirectories != null) {
             for (File nestedElement : nestedFilesAndDirectories) {
-                addToJar(nestedElement, target);
+                addToJar(nestedElement, target, excludeFromPath, extensionToIgnore);
             }
         }
     }
 
-    private void writeToJar(InputStream sourceStream, JarOutputStream target) throws IOException {
+    private void writeDirectoryJarEntry(File directory, JarOutputStream target, String excludeFromPath) throws IOException {
+        String directoryPathInJarFormat = convertPathToJarFormat(directory.getPath(), excludeFromPath);
+        JarEntry entry = new JarEntry(directoryPathInJarFormat);
+        entry.setTime(directory.lastModified());
+        try {
+            target.putNextEntry(entry);
+        } catch (ZipException duplicateEntryException) {
+            // ignore -- duplicate
+        }
+        target.closeEntry();
+    }
+
+    private void addFileToJar(JarOutputStream target, File file, String excludeFromPath) throws IOException {
+        String filePathInJarFormat = convertPathToJarFormat(file.getPath(), excludeFromPath);
+        JarEntry entry = new JarEntry(filePathInJarFormat);
+        entry.setTime(file.lastModified());
+        try {
+            target.putNextEntry(entry);
+            writeFileToJar(new FileInputStream(file), target);
+        } catch (ZipException duplicateEntryException) {
+            // ignore -- duplicate
+        }
+        target.closeEntry();
+    }
+
+    private void writeFileToJar(InputStream sourceStream, JarOutputStream target) throws IOException {
         InputStream in = new BufferedInputStream(sourceStream);
         byte[] buffer = new byte[MAX_ENTRY_LENGTH];
         int count;
@@ -112,57 +167,12 @@ public class Packager {
         in.close();
     }
 
-    private String convertPathToJarFormat(String path) {
-        return path.replaceAll(WINDOWS_PATH_DELIMITER_PATTERN, File.separator);
-    }
 
-    private void writeResources(JarOutputStream target) throws IOException {
-        Queue<File> fileQueue = new LinkedList<>(Arrays.asList(new File
-                ("out/production/voogasalad_duvallinthistogether").listFiles()));
-        while(!fileQueue.isEmpty()){
-            File resourceOrResourceDirectory = fileQueue.poll();
-            if (resourceOrResourceDirectory.isDirectory()) {
-                fileQueue.addAll(Arrays.asList(resourceOrResourceDirectory.listFiles()));
-            } else if (!resourceOrResourceDirectory.getPath().endsWith("md") &&
-                    !resourceOrResourceDirectory.getPath().endsWith(".class")) {
-                writeFileToJar(target, resourceOrResourceDirectory, "out/production/voogasalad_duvallinthistogether/");
-            }
+    private String convertPathToJarFormat(String path, String partOfPathToExclude) {
+        String convertedPath = path.replaceAll(WINDOWS_PATH_DELIMITER_PATTERN, File.separator);
+        if (!convertedPath.endsWith(File.separator)) {
+            convertedPath += File.separator;
         }
-    }
-
-    private void writeFileToJar(JarOutputStream target, File file, String replace) throws IOException {
-        JarEntry entry = new JarEntry(convertPathToJarFormat(file.getPath().replace(replace, "")));
-        addEntryToJar(target, file, entry);
-    }
-
-    private void addEntryToJar(JarOutputStream target, File entrySource, JarEntry entry) throws IOException {
-        entry.setTime(entrySource.lastModified());
-        target.putNextEntry(entry);
-        writeToJar(new FileInputStream(entrySource), target);
-        target.closeEntry();
-    }
-
-    private void addLibraries(JarOutputStream target) throws IOException {
-        for (File libraryFile : new File("lib").listFiles()) {
-            if (libraryFile.getPath().endsWith(".jar")) {
-                JarFile jarFile = new JarFile(libraryFile);
-                Stream<JarEntry> jarEntryStream = jarFile.stream();
-                jarEntryStream.forEach(jarEntry -> addLibraryJarEntry(target, jarFile, jarEntry));
-
-            }
-        }
-    }
-
-    private void addLibraryJarEntry(JarOutputStream target, JarFile jarFile, JarEntry jarEntry) {
-        try {
-            if (!jarEntry.getName().endsWith(".MF")) {
-                target.putNextEntry(jarEntry);
-                writeToJar(jarFile.getInputStream(jarEntry), target);
-                target.closeEntry();
-            }
-        } catch (IOException ioException) {
-            // ignore
-            ioException.printStackTrace();
-        }
+        return convertedPath.replace(partOfPathToExclude, "");
     }
 }
