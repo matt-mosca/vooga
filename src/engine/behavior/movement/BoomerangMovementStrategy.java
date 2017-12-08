ca76fab52ad5ce8f0042e8fc727ae86cdb31bc25
Michael Scruggs <mts40@duke.edu>
2017-12-08 16:15:11 -0500

    Added Query testing

---
+++ src/engine/behavior/movement/BoomerangMovementStrategy.java
@@ -0,0 +1,11 @@
+package engine.behavior.movement;
+
+public class BoomerangMovementStrategy extends CircularMovementStrategy{
+
+	private final
+	public BoomerangMovementStrategy(double targetX, double targetY,double velocity) {
+		super(centerX, centerY, radius, initialAngle, velocity);
+		// TODO Auto-generated constructor stub
+	}
+
+}
