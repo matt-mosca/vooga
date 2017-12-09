1d39cb03deb1b5abe65ddbdf32a9085d2c66609d
Venkat Subramaniam <venkat.subramaniam@duke.edu>
2017-12-09 18:11:57 -0500

    changing how lvDisplays work

---
+++ src/authoring/bottomToolBar/LevelsEditDisplay.java
@@ -0,0 +1,30 @@
+package authoring.bottomToolBar;
+
+import engine.authoring_engine.AuthoringController;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class LevelsEditDisplay {
+	private static final int SIZE = 800;
+	
+	private Stage myStage;
+	private Scene myScene;
+	private AuthoringController myController;
+	private BorderPane myRoot;
+	private ResourceDisplay resourceEditor;
+	private GameEnder gameEnder;
+	
+	public LevelsEditDisplay(AuthoringController controller) {
+		myController = controller;
+		myStage = new Stage();
+		myRoot = new BorderPane();
+		myScene = new Scene(myRoot, SIZE, SIZE);
+		resourceEditor = new ResourceDisplay(controller);
+		gameEnder = new GameEnder(controller);
+		myRoot.setLeft(gameEnder);
+		myRoot.setRight(resourceEditor);
+		myStage.show();
+	}
+}
