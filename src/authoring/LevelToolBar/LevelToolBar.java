9af767bc2d270e8aeafaa9d74d77c19dd9e7fc06
Matt O <mo121@duke.edu>
2017-12-10 00:24:08 -0500

    Levels and waves

--- src/authoring/LevelToolBar/LevelToolBar.java
+++ src/authoring/LevelToolBar/LevelToolBar.java
@@ -3,6 +3,8 @@ package authoring.LevelToolBar;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import authoring.EditDisplay;
 import authoring.GameArea;
@@ -15,6 +17,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 import util.protocol.ClientMessageUtils;
 import display.sprites.InteractiveObject;
@@ -28,6 +31,7 @@ public class LevelToolBar extends VBox {
 	private List<GameArea> myGameAreas;
 	private List<List<ImageView>> mySprites;
 	private ScrollableArea myScrollableArea;
+	private WaveDisplay myWaveDisplay;
 	private TabFactory tabMaker;
 	private final int X_LAYOUT = 260;
 	private final int Y_LAYOUT = 470;
@@ -37,10 +41,12 @@ public class LevelToolBar extends VBox {
 	private EditDisplay myCreated;
 	private SpriteDisplayer mySpriteDisplay;
 	private LevelsEditDisplay myLevelDisplayer;
+	private Map<Integer, Integer> wavesPerLevel;
 
 	private ClientMessageUtils clientMessageUtils;
 
 	public LevelToolBar(EditDisplay created, AuthoringController controller, ScrollableArea area) {
+		
 		myScrollableArea = area;
 		currentDisplay = 1;
 		myCreated = created;
@@ -54,10 +60,15 @@ public class LevelToolBar extends VBox {
 		mySprites = new ArrayList<>();
 		mySprites.add(new ArrayList<>());
 		newLevel = new Button("New Level");
+		Button newWaveButton = new Button("New Wave");
+		wavesPerLevel = new TreeMap<Integer, Integer>();
 		newLevel.setOnAction(e -> addLevel());
+		newWaveButton.setOnAction(e->newWaveButtonPressed());
 		myTabPane = new TabPane();
 		tabMaker = new TabFactory();
 		mySpriteDisplay = new SpriteDisplayer();
+		myWaveDisplay = new WaveDisplay();
+		this.getChildren().add(myWaveDisplay);
 		this.getChildren().add(mySpriteDisplay);
 		myTabPane.setMaxSize(400, 100);
 		myTabPane.setPrefSize(400, 100);
@@ -72,13 +83,23 @@ public class LevelToolBar extends VBox {
 		this.getChildren().add(myTabPane);
 		this.getChildren().add(newLevel);
 		this.getChildren().add(editLevel);
+		this.getChildren().add(newWaveButton);
 		loadLevels();
 		created.setGameArea(myGameAreas.get(0));
+	}
+	
+	private void newWaveButtonPressed() {
+		wavesPerLevel.put(currentDisplay, wavesPerLevel.get(currentDisplay)+1);
+		updateWaveDisplay();
 	}
 
 	private void openLevelDisplay() {
 		myLevelDisplayer = new LevelsEditDisplay(myController, myCreated);
 		myLevelDisplayer.open();
+	}
+	
+	private void updateWaveDisplay() {
+		myWaveDisplay.addTabs(wavesPerLevel.get(currentDisplay));
 	}
 
 	private void loadLevels() {
@@ -92,7 +113,7 @@ public class LevelToolBar extends VBox {
 			initializeSprites(i);
 		}
 	}
-
+	
 	private void addLevel() {
 		mySprites.add(new ArrayList<>());
 		Tab newTab = tabMaker.buildTabWithoutContent("Level " + Integer.toString(myLevels.size() + 1), null, myTabPane);
@@ -100,6 +121,7 @@ public class LevelToolBar extends VBox {
 		LevelTab newLv = new LevelTab(myLevels.size() + 1, myController);
 		myGameAreas.add(new GameArea(myController));
 		myController.setLevel(myLevels.size() + 1);
+		wavesPerLevel.put(myLevels.size()+1, 1);
 		if (myLevels.size() == 0) {
 			newTab.setClosable(false);
 		} else {
@@ -109,7 +131,6 @@ public class LevelToolBar extends VBox {
 		newLv.attach(newTab);
 		myLevels.add(newLv);
 		myTabPane.getTabs().add(newTab);
-
 	}
 
 	// TODO need load in static object rather than just imageview
@@ -135,6 +156,7 @@ public class LevelToolBar extends VBox {
 		myController.setLevel(i);
 		myCreated.setGameArea(myGameAreas.get(i - 1));
 		updateSpriteDisplay(i);
+		updateWaveDisplay();
 	}
 
 	private void deleteLevel(int lvNumber) {
