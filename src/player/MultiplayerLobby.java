bdfb9b45571e7a63f5cdd142c0d26dce0a6b5f18
Adithya <ra102@duke.edu>
2017-12-12 21:09:43 -0500

    Integrate push notifications with MultiPlayer lobby, front end display of notification changes TODO by Mosca

--- src/player/MultiplayerLobby.java
+++ src/player/MultiplayerLobby.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import display.factory.ButtonFactory;
 import display.splashScreen.ScreenDisplay;
+import javafx.collections.ListChangeListener.Change;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextInputDialog;
@@ -17,14 +18,13 @@ import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 import networking.MultiPlayerClient;
 import networking.protocol.PlayerServer.LevelInitialized;
-import util.io.SerializationUtils;
+import networking.protocol.PlayerServer.Notification;
 
 public class MultiplayerLobby extends ScreenDisplay {
-//	private static final String BACKGROUND_IMAGE = "grass_large.png";
+	// private static final String BACKGROUND_IMAGE = "grass_large.png";
 	private static final String BACKGROUND_IMAGE = "space_background.jpeg";
-	
+
 	private PlayDisplay playDisplay;
-	private BorderPane multiplayerLayout;
 	private Label topScreenLabel;
 	private Label lobbyPlayersLabel;
 	private Label lobbiesLabel;
@@ -49,16 +49,21 @@ public class MultiplayerLobby extends ScreenDisplay {
 	private String username;
 	private String gameName;
 	private String currentLobby;
+	private boolean launched = false; // To avoid duplicate launching from notification broadcast if player was the
+										// launcher
 
-	//Add a way to select a lobby in the list and then press "Join Selected Lobby" to join it
-	//To do this, make an EventHandler- upon clicking, reset an instance variable
-	//Clicking Join Selected Lobby button will then join the lobby specified by that variable
-	
-	//TODO need to start the play display at some point when you are ready!
-	
-	public MultiplayerLobby(int width, int height, Paint background, Stage currentStage, PlayDisplay play, MultiPlayerClient multiPlayerClient) {
+	// Add a way to select a lobby in the list and then press "Join Selected Lobby"
+	// to join it
+	// To do this, make an EventHandler- upon clicking, reset an instance variable
+	// Clicking Join Selected Lobby button will then join the lobby specified by
+	// that variable
+
+	// TODO need to start the play display at some point when you are ready!
+
+	public MultiplayerLobby(int width, int height, Paint background, Stage currentStage, PlayDisplay play,
+			MultiPlayerClient multiPlayerClient) {
 		super(width, height, background, currentStage);
-//		multiplayerLayout = new BorderPane();
+		// multiplayerLayout = new BorderPane();
 		playDisplay = play;
 		topScreenLabel = new Label();
 		lobbyPlayersLabel = new Label();
@@ -74,12 +79,13 @@ public class MultiplayerLobby extends ScreenDisplay {
 		lobbies = new MultiplayerListBox();
 		players = new MultiplayerListBox();
 		multiClient = multiPlayerClient;
+		registerNotificationListener();
 		username = new String();
 		gameName = new String();
 		currentLobby = new String();
 		gameName = playDisplay.getGameState();
 		setMultiplayerBackground(width, height);
-//		rootAdd(multiplayerLayout);
+		// rootAdd(multiplayerLayout);
 		rootAdd(topScreenLabel);
 		createButtons();
 		setUpButtonBox();
@@ -89,7 +95,7 @@ public class MultiplayerLobby extends ScreenDisplay {
 		createGameStateLabel(width, height);
 		setStyleAndLayout(width, height);
 	}
-	
+
 	public void promptForUsername() {
 		TextInputDialog dialog = new TextInputDialog("Matthew");
 		dialog.setTitle("Name Selection");
@@ -97,17 +103,17 @@ public class MultiplayerLobby extends ScreenDisplay {
 		dialog.setContentText("Username:");
 
 		Optional<String> result = dialog.showAndWait();
-		if (result.isPresent()){
-		    username = result.get();
+		if (result.isPresent()) {
+			username = result.get();
 		}
 		setUsername();
 	}
-	
+
 	private void setUsername() {
 		usernameLabel.setText(username);
 		usernameBox.getChildren().add(usernameLabel);
 	}
-	
+
 	private void setStyleAndLayout(int width, int height) {
 		rootStyleAndClear("player/resources/multiplayer.css");
 		topScreenLabel.getStyleClass().add("label2");
@@ -140,7 +146,7 @@ public class MultiplayerLobby extends ScreenDisplay {
 		usernameBox.setSpacing(10);
 		lobbiesListBox.getStyleClass().add("borders");
 	}
-	
+
 	private void setMultiplayerBackground(int width, int height) {
 		String backgroundName = BACKGROUND_IMAGE;
 		Image image = new Image(getClass().getClassLoader().getResourceAsStream(backgroundName));
@@ -149,20 +155,20 @@ public class MultiplayerLobby extends ScreenDisplay {
 		splashBackground.setFitHeight(height);
 		rootAdd(splashBackground);
 	}
-	
+
 	private void initializeMultiplayerHomeScreen() {
 		setTopLabelForMultiplayerHomeScreen();
 		addButtonBox();
 	}
-	
+
 	private void initializeLobbiesList() {
 		addLobbiesListBox();
 		addBack();
 		addJoinSelectedLobby();
 		lobbies.setNames(multiClient.getGameRooms());
 	}
-	
-	//Eventually should take in parameter of lobby name
+
+	// Eventually should take in parameter of lobby name
 	private void initializeLobby() {
 		setTopLabelForLobby();
 		addPlayersBox();
@@ -171,81 +177,81 @@ public class MultiplayerLobby extends ScreenDisplay {
 		addReturnToLobbies();
 		players.setNames(multiClient.getPlayerNames());
 	}
-	
+
 	private void clearMultiplayerHomeScreen() {
 		removeButtonBox();
 	}
-	
+
 	private void clearLobbiesList() {
 		removeLobbiesListBox();
 		removeBack();
 		removeJoinSelectedLobby();
 	}
-	
+
 	private void clearLobby() {
 		removePlayersBox();
 		removeStartGame();
 		removeReturn();
 		removeReturnToLobbies();
 	}
-	
+
 	private void changeHomeToLobbiesList() {
 		clearMultiplayerHomeScreen();
 		initializeLobbiesList();
 	}
-	
+
 	private void changeLobbiesListToLobby() {
 		clearLobbiesList();
 		initializeLobby();
 	}
-	
+
 	private void changeHomeToLobby() {
 		clearMultiplayerHomeScreen();
 		initializeLobby();
 	}
-	
+
 	private void changeLobbiesListToHome() {
 		clearLobbiesList();
 		initializeMultiplayerHomeScreen();
 	}
-	
+
 	private void changeLobbyToHome() {
 		clearLobby();
 		initializeMultiplayerHomeScreen();
 		multiClient.exitGameRoom();
 	}
-	
+
 	private void changeLobbyToLobbiesList() {
 		clearLobby();
 		initializeLobbiesList();
 		multiClient.exitGameRoom();
 	}
-	
+
 	private void createLobby() {
 		promptForLobbyName();
 		multiClient.createGameRoom(gameName, currentLobby);
 		System.out.println(currentLobby);
 		multiClient.joinGameRoom(currentLobby, username);
-//		multiClient.joinGameRoom("mosca_dope_game", username);
-//		multiClient.joinGameRoom("circularMonkey.voog_2", username);
+		// multiClient.joinGameRoom("mosca_dope_game", username);
+		// multiClient.joinGameRoom("circularMonkey.voog_2", username);
 		changeHomeToLobby();
 	}
-	
+
 	private void joinLobby() {
 		String selectedLobby = getSelectedLobby();
-		if(selectedLobby != null)
+		if (selectedLobby != null)
 			currentLobby = selectedLobby;
 		multiClient.joinGameRoom(currentLobby, username);
 		changeHomeToLobbiesList();
 	}
-	//getSelectionModel.getSelectedItem
-	
+	// getSelectionModel.getSelectedItem
+
 	private String getSelectedLobby() {
 		return lobbies.getListView().getSelectionModel().getSelectedItem();
 	}
-	
-	//Call this method when user wants to create new lobby- to name the lobby
-	//This method should pull up prompt box
+
+	// Call this method when user wants to create new lobby- to name the lobby
+	// This method should pull up prompt box
 	private void promptForLobbyName() {
 		TextInputDialog dialog = new TextInputDialog("Cool lobby");
 		dialog.setTitle("Lobby Name");
@@ -253,28 +259,30 @@ public class MultiplayerLobby extends ScreenDisplay {
 		dialog.setContentText("Lobby name:");
 
 		Optional<String> result = dialog.showAndWait();
-		if (result.isPresent()){
-		    currentLobby = result.get();
-		    System.out.println(currentLobby);
+		if (result.isPresent()) {
+			currentLobby = result.get();
+			System.out.println(currentLobby);
 		}
 	}
-	
-	//This method or an external method would be called when "START GAME" is pressed (serves as action for button)
+
+	// This method or an external method would be called when "START GAME" is
+	// pressed (serves as action for button)
 	private void startGame() {
 		LevelInitialized newLevelData = multiClient.launchGameRoom();
+		launched = true;
 		getStage().setScene(playDisplay.getScene());
 		playDisplay.startDisplay(newLevelData);
-//		multiClient.launchGameRoom(currentLobby);
+		// multiClient.launchGameRoom(currentLobby);
 	}
-	
+
 	private void setTopLabelForLobby() {
 		topScreenLabel.setText("VOOGA Lobby");
 	}
-	
+
 	private void setTopLabelForMultiplayerHomeScreen() {
 		topScreenLabel.setText("Multiplayer");
 	}
-	
+
 	private void createButtons() {
 		createGameLobby = buttonFactory.buildDefaultTextButton("Create Game Lobby", e -> createLobby());
 		joinGameLobby = buttonFactory.buildDefaultTextButton("Join Game Lobby", e -> joinLobby());
@@ -282,25 +290,26 @@ public class MultiplayerLobby extends ScreenDisplay {
 		returnHome = buttonFactory.buildDefaultTextButton("Return to Multiplayer Home", e -> changeLobbyToHome());
 		backHome = buttonFactory.buildDefaultTextButton("Back", e -> changeLobbiesListToHome());
 		returnToLobbies = buttonFactory.buildDefaultTextButton("View Open Lobbies", e -> changeLobbyToLobbiesList());
-		joinSelectedLobby = buttonFactory.buildDefaultTextButton("Join Selected Lobby", e -> changeLobbiesListToLobby());
+		joinSelectedLobby = buttonFactory.buildDefaultTextButton("Join Selected Lobby",
+				e -> changeLobbiesListToLobby());
 	}
-	
+
 	private void createGameStateLabel(int width, int height) {
-//		VBox gameStateBox = new VBox();
-//		gameStateBox.getStyleClass().add("borders");
-//		gameLabel
-//		gameStateBox.getChildren().add(gameStateLabel);
-//		gameStateBox.setLayoutY(height - 80);
+		// VBox gameStateBox = new VBox();
+		// gameStateBox.getStyleClass().add("borders");
+		// gameLabel
+		// gameStateBox.getChildren().add(gameStateLabel);
+		// gameStateBox.setLayoutY(height - 80);
 		gameStateLabel.setText("Game:  " + gameName);
 		gameStateLabel.setLayoutY(height - 40);
 		rootAdd(gameStateLabel);
 	}
-	
+
 	private void setUpButtonBox() {
 		buttonBox.getChildren().add(createGameLobby);
 		buttonBox.getChildren().add(joinGameLobby);
 	}
-	
+
 	private void setUpRightBox() {
 		String imageName = "person_silhouette.png";
 		Image personImage = new Image(getClass().getClassLoader().getResourceAsStream(imageName));
@@ -315,99 +324,140 @@ public class MultiplayerLobby extends ScreenDisplay {
 		players.attach(playersBox);
 		rootAdd(rightBox);
 	}
-	
+
 	private void setUpLobbiesListBox() {
 		lobbiesLabel.setText("Lobbies");
 		lobbiesListBox.getChildren().add(lobbiesLabel);
 		lobbies.attach(lobbiesListBox);
 	}
-	
+
 	private void addButtonBox() {
-//		centerBox.getChildren().add(buttonBox);
+		// centerBox.getChildren().add(buttonBox);
 		rootAdd(buttonBox);
 	}
-	
+
 	private void removeButtonBox() {
-//		centerBox.getChildren().remove(buttonBox);
+		// centerBox.getChildren().remove(buttonBox);
 		rootRemove(buttonBox);
 	}
 
 	private void addPlayersBox() {
 		rightBox.getChildren().add(playersBox);
-//		rootAdd(playersBox);
+		// rootAdd(playersBox);
 	}
-	
+
 	private void removePlayersBox() {
 		rightBox.getChildren().remove(playersBox);
-//		rootRemove(playersBox);
+		// rootRemove(playersBox);
 	}
-	
+
 	private void addStartGame() {
-//		bottomBox.getChildren().add(startGame);
+		// bottomBox.getChildren().add(startGame);
 		rootAdd(startGame);
 	}
-	
+
 	private void removeStartGame() {
-//		bottomBox.getChildren().remove(startGame);
+		// bottomBox.getChildren().remove(startGame);
 		rootRemove(startGame);
 	}
-	
+
 	private void addReturn() {
-//		rightBox.getChildren().add(returnHome);
+		// rightBox.getChildren().add(returnHome);
 		rootAdd(returnHome);
 	}
-	
+
 	private void removeReturn() {
-//		rightBox.getChildren().remove(returnHome);
+		// rightBox.getChildren().remove(returnHome);
 		rootRemove(returnHome);
 	}
-	
+
 	private void addBack() {
-//		rightBox.getChildren().add(returnHome);
+		// rightBox.getChildren().add(returnHome);
 		rootAdd(backHome);
 	}
-	
+
 	private void removeBack() {
-//		rightBox.getChildren().remove(returnHome);
+		// rightBox.getChildren().remove(returnHome);
 		rootRemove(backHome);
 	}
-	
+
 	private void addReturnToLobbies() {
-//		rightBox.getChildren().add(returnHome);
+		// rightBox.getChildren().add(returnHome);
 		rootAdd(returnToLobbies);
 	}
-	
+
 	private void removeReturnToLobbies() {
-//		rightBox.getChildren().remove(returnHome);
+		// rightBox.getChildren().remove(returnHome);
 		rootRemove(returnToLobbies);
 	}
-	
+
 	private void addLobbiesListBox() {
 		rootAdd(lobbiesListBox);
 	}
-	
+
 	private void removeLobbiesListBox() {
 		rootRemove(lobbiesListBox);
 	}
-	
+
 	private void addJoinSelectedLobby() {
 		rootAdd(joinSelectedLobby);
 	}
-	
+
 	private void removeJoinSelectedLobby() {
 		rootRemove(joinSelectedLobby);
 	}
-	
+
+	private void registerNotificationListener() {
+		multiClient.registerNotificationListener(notification -> processNotification(notification));
+	}
+
+	private void processNotification(Change<? extends Notification> notification) {
+		System.out.println("Processing notification in lobby!");
+		while (notification.next()) {
+			notification.getAddedSubList().stream().forEach(message -> {
+				if (message.hasPlayerJoined()) {
+					String nameOfJoinedPlayer = message.getPlayerJoined().getUserName();
+					handleUserJoinedRoom(nameOfJoinedPlayer);
+				}
+				if (message.hasPlayerExited()) {
+					String nameOfExitedPlayer = message.getPlayerExited().getUserName();
+					handleUserExitedRoom(nameOfExitedPlayer);
+				}
+				if (message.hasLevelInitialized() && !launched) {
+					LevelInitialized levelData = message.getLevelInitialized();
+					handleGameLaunched(levelData);
+				}
+			});
+		}
+	}
+
+	// TODO - Mosca : add user to display and add some message?
+	private void handleUserJoinedRoom(String userName) {
+		System.out.println("User " + userName + " joined!");
+	}
+
+	// TODO - Mosca : remove user from display and add some message?
+	private void handleUserExitedRoom(String userName) {
+		System.out.println("User " + userName + "exited!");
+	}
+
+	// TODO - Mosca (Check if correct)
+	private void handleGameLaunched(LevelInitialized levelData) {
+		launched = true;
+		getStage().setScene(playDisplay.getScene());
+		playDisplay.startDisplay(levelData);
+	}
+
 	@Override
 	public void save() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void listItemClicked(MouseEvent e, ImageView object) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
